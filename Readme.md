e05b3cb316a300617f2a3feaf3be811ad7637fe4
Jamire44 <2003doyle@gmail.com>
2023-07-09 18:40:44 +0100

    Added comments to follow

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# This calculator will prompt the user to select what shape they would like to see the Area and Perimeter of the shape.
+# They would get to type in the dimensions and the calculator will tell them the exact measurements to 2 decimal places.
+# This will also include error handling, making sure the user enters a valid input.
---
+++ area-perimeter-calc.py
@@ -0,0 +1,25 @@
+import math
+
+
+# Shape Selection: Users can choose a shape from a list (rectangle, circle, triangle) to calculate its area and perimeter accurately.
+
+
+# Dimension Input: Users are prompted to enter the necessary dimensions for the selected shape (length and width for a rectangle, radius for a circle, base and height for a triangle) to ensure accurate calculations.
+
+
+# Calculation Logic: The program accurately calculates the area and perimeter of the selected shape using the appropriate formulas.
+
+
+# Results Display: The calculated area and perimeter values are displayed clearly, including appropriate units of measurement, to facilitate easy interpretation.
+
+
+# Error Handling: The program handles errors gracefully, providing clear error messages if users enter invalid dimensions and allowing them to correct the input for accurate calculations.
+
+
+# User-Friendly Interface: The program features an intuitive user interface with clear instructions and prompts, ensuring a seamless user experience.
+
+
+# Well-Structured Code: The code is well-structured and modular, with meaningful variable names and comments, enhancing readability and maintainability.
+
+
+#Comprehensive User Guide: A detailed user guide is provided, explaining how to effectively use the program, including step-by-step instructions for shape selection, dimension input, and result interpretation.
