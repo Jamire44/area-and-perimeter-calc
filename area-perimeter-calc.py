ab5408fe25ef4918c1ad071e2ad18f6901675a61
Jamire44 <2003doyle@gmail.com>
2023-07-09 21:21:04 +0100

    Added a try except and put that into a function

--- area-perimeter-calc.py
+++ area-perimeter-calc.py
@@ -1,6 +1,10 @@
 import math as M
 
+
+
 class Shapes:
+
+    
 
     def circleArea(self, r):
         self.r = r
@@ -42,36 +46,45 @@ class Shapes:
         per = 2 * (l + w)
         print(f"The perimeter of the rectangle is {per}")
 
-
+def validate_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 shape = Shapes()
 
-o = True
 
-while o:
+
+while True :
     print("1. Circle")
     print("2. Square")
     print("3. Rectangle")
+    print("4. Exit")
+
 
     choice = input("Please choose which shape you would like to see the Area and Perimeter of : ")
     
-    if choice == "1" and choice.isdigit():
-            r = float(input("Please enter the radius of the circle: "))
+    if choice.isdigit():
+        if choice == "1":
+            r = validate_input("Please enter the radius of the circle: ")
             shape.circleArea(r)
             shape.circlePerimeter(r)
-            
-    elif choice == "2" and choice.isdigit():
-            a = float(input("Please enter the length of one side in the square: "))
+        elif choice == "2":
+            a = validate_input("Please enter the length of one side in the square: ")
             shape.squareArea(a)
             shape.squarePerimeter(a)
-
-    elif choice == "3" and choice.isdigit():
-            l = float(input("Please enter the Length of the rectangle:"))
-            w = float(input("Please enter the Width of the rectangle:"))
-
+        elif choice == "3":
+            l = validate_input("Please enter the Length of the rectangle: ")
+            w = validate_input("Please enter the Width of the rectangle: ")
             shape.rectanglearea(l, w)
             shape.rectanglePerimeter(l, w)
-
-    else: 
-            print("You have enter an invalid number")
-
+        elif choice == "4":
+            print("Exiting...")
+            quit()
+        else:
+            print("You have entered an invalid number")
+    else:
+        print("Number is not a digit")
