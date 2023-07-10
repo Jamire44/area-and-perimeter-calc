6a2816ef50058c668b1e0f55e6f067b46711c0b9
Jamire44 <2003doyle@gmail.com>
2023-07-11 00:19:19 +0100

    changed func to classes

--- area-perimeter-calc.py
+++ area-perimeter-calc.py
@@ -1,107 +1,55 @@
+import unittest
 import math as M
 
-
 # Class for all of the Area and Perimeter calculations
-class Shapes:
 
-    def circleArea(self, radius):
+class Shapes:
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(Self):
+        pass
+
+class Circle(Shapes):
+
+    def __init__(self, radius):
         self.radius = radius
 
-        area = M.pi * (radius * radius)
+    def calculate_area(self):
+        area = M.pi * (self.radius * self.radius)
         print(f"\nThe area of the circle is {area}\n")
 
-    def circlePerimeter(self, radius):
-        self.radius = radius
-
-        perimeter = 2 * M.pi * radius
+    def calculate_perimeter(self):
+        perimeter = 2 * M.pi * self.radius
         print(f"\nThe perimeter of the circle is {perimeter}\n")
 
+class Square(Shapes):
 
-    def squareArea(self, length):
+    def __init__(self, length):
         self.length = length
+        
 
-        area = length * length
+    def calculate_area(self):
+        area = self.length * self.length
         print(f"\nThe area of the square is {area}\n")
         
-    def squarePerimeter(self, length):
-        self.length = length
+    def calculate_perimeter(self):
+        self.length = self.length
         
-        perimeter = length * 4
+        perimeter = self.length * 4
         print(f"\nThe perimeter of the square is {perimeter}\n")
 
-    def rectanglearea(self, length, width):
+class Rectangle(Shapes):
+
+    def __init__(self, length, width):
         self.length = length
         self.width = width
 
-        area = width * length
+    def calculate_area(self):
+        area = self.width * self.length
         print(f"\nThe area of this rectange is {area}\n")
 
 
-    def rectanglePerimeter(self, length, width):
-        self.width = width
-        self.length = length
-
-        perimeter = 2 * (length + width)
+    def calculate_perimeter(self):
+        perimeter = 2 * (self.length + self.width)
         print(f"The perimeter of the rectangle is {perimeter}\n")
-
-
-# Function that has a while loop to iterate over the answer of the user, if a number isnt entered the user will be prompt to try again
-def logic():
-    while True :
-        print("1. Circle")
-        print("2. Square")
-        print("3. Rectangle")
-        print("4. Exit")
-
-
-        # Prompting the user to pick a shape
-        choice = input("\nPlease choose which shape you would like to see the Area and Perimeter of (Make sure you select the number of the shape you would like to choose): ")
-        
-
-        # Checks if what is entered is a digit
-        if choice.isdigit():
-
-            # If-elif-else to do what the user selects
-            if choice == "1":
-                radius = validate_input("\nPlease enter the radius of the circle: ")
-                shape.circleArea(radius)
-                shape.circlePerimeter(radius)
-
-            elif choice == "2":
-                area = validate_input("\nPlease enter the length of one side in the square: ")
-                shape.squareArea(area)
-                shape.squarePerimeter(area)
-
-            elif choice == "3":
-                length = validate_input("\nPlease enter the Length of the rectangle: ")
-                width = validate_input("\nPlease enter the Width of the rectangle: ")
-                shape.rectanglearea(length, width)
-                shape.rectanglePerimeter(length, width)
-
-            elif choice == "4":
-                print("Exiting...")
-                quit()
-
-            else:
-                print("\nYou have entered an invalid number\n")
-
-        else:
-            print("Number is not a digit\n")
-
-
-
-# Function to check if the user entered a number.
-# If user entered a number, Number will be allowed
-# If user entered something else, They will be prompted to enter a valid number
-def validate_input(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except ValueError:
-            print("Invalid input. Please enter a valid number.\n")
-
-
-shape = Shapes()
-
-logic()
